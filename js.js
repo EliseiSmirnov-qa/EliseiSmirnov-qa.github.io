19c89b7a892e47145738db771c5422793b82bad5
Elisei Smirnov <125597167+EliseiSmirnov-qa@users.noreply.github.com>
2023-02-25 17:09:20 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Путешествуй, оно того стоит! 😉')
+	alert('Путешествуй, оно того стоит! 😉✈️')
 })
